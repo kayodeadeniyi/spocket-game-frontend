86c4bcd979757e4c7e63448f81d44aa91a7e7dd7
Kayode Adeniyi <kayode.adeniyi@andela.co>
2019-05-28 13:28:04 +0100

    Add eslint

---
+++ src/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: "react-app",
+  rules: {
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+    "jsx-quotes": [1, "prefer-single"],
+    "react/require-default-props": [0, { forbidDefaultForRequired: true }],
+    "react/destructuring-assignment": [0],
+    semi: [0, "never"],
+    quotes: [2, "single"],
+    "comma-dangle": [2, "never"],
+    "max-len": [0],
+    "react/button-has-type": [0]
+  }
+};
